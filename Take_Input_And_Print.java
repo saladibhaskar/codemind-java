5a01529968cc9be3719cc342f9b2c5fd43027675
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2023-08-13 17:00:33 +0530

    import java.util.Scanner; public class Iando {     public static void main(String[] Bhaskar)     {         Scanner s=new Scanner(System.in);         String a=s.nextLine();         System.out.print(a);
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Iando
+{
+    public static void main(String[] Bhaskar)
+    {
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        System.out.print(a);
+        
+    }
+}
