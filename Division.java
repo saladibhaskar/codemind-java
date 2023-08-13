b25f05bbbf59f3fc6280cd3569dd724d705c707f
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2023-08-13 16:54:19 +0530

    import java.util.Scanner; public class Div {     public static void main(String[]  bhaskar)
        {
            Scanner s=new Scanner(System.in);
        
            int a=s.nextInt();
            int b=s.nextInt();
            System.out.print(a/b);
        }
        
    }

---
+++ Division.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Div
+{
+    public static void main(String[]  bhaskar)
+    
+    {
+        Scanner s=new Scanner(System.in);
+    
+        int a=s.nextInt();
+        int b=s.nextInt();
+        System.out.print(a/b);
+    }
+    
+}
