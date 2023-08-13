5df231b4afd1b19a071424c980cc1aa5a50d7f6a
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2023-08-13 16:46:08 +0530

    import java.util.Scanner; public class cca {     public static void main(String[] sag)     {         Scanner s=new Scanner(System.in);         {             int a=s.nextInt();             int b=s.nextInt();             int c=s.nextInt();             System.out.print(a*b*c*512*2);         }     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class cca
+{
+    public static void main(String[] sag)
+    {
+        Scanner s=new Scanner(System.in);
+        {
+            int a=s.nextInt();
+            int b=s.nextInt();
+            int c=s.nextInt();
+            System.out.print(a*b*c*512*2);
+        }
+    }
+}
