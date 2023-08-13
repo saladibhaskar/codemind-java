95878802f563c6dcf552a6e632f69f2b93490655
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2023-08-13 17:06:33 +0530

    import java.util.Scanner; public class New {     public static void main(String[] Bhaskar )     {           Scanner s=new Scanner(System.in);           int a=s.nextInt();           int b=s.nextInt();           System.out.print(a%b);     } }

---
+++ Modulus.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class New
+{
+    public static void main(String[] Bhaskar )
+    {
+          Scanner s=new Scanner(System.in);
+          int a=s.nextInt();
+          int b=s.nextInt();
+          System.out.print(a%b);
+    }
+}
