f84bf7b359ece723910901fc4b5754821e4dbbfe
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2023-08-17 11:29:40 +0530

    import java.util.Scanner; public class He {     public static void main(String[] sb)     {         Scanner s=new Scanner(System.in);         int t=s.nextInt();         for(int i=1;i<=t;i++)         {             String n=s.next();             int a=Integer.parseInt(n,8);             String b=Integer.toString(a,2);             System.out.println(b);             /*int len=b.length();             if(len%4==0)             {             System.out.println(b);
                }
                else if (len%4==1)
                {
                    System.out.println("000"+b);
                    
                }
                else if (len%4==2)
                {
                    System.out.println("00"+b);
                    
                }else if (len%4==3)
                {
                    System.out.println("0"+b);
                    
                }*/
                }
            }
        }

---
+++ Octal_to_Binary.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class He
+{
+    public static void main(String[] sb)
+    {
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=1;i<=t;i++)
+        {
+            String n=s.next();
+            int a=Integer.parseInt(n,8);
+            String b=Integer.toString(a,2);
+            System.out.println(b);
+            /*int len=b.length();
+            if(len%4==0)
+            {
+            System.out.println(b);
+                
+            }
+            else if (len%4==1)
+            {
+                System.out.println("000"+b);
+                
+            }
+            else if (len%4==2)
+            {
+                System.out.println("00"+b);
+                
+            }else if (len%4==3)
+            {
+                System.out.println("0"+b);
+                
+            }*/
+            }
+        }
+    }
